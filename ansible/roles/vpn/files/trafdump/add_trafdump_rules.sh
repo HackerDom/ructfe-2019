a79a4ca310cc910aaf46498b86ff3446f44b4913
Alexander Bersenev <bay@hackerdom.ru>
2019-11-14 04:23:42 +0500

    deploy: vpn role trafdump and connectmon

--- ansible/roles/vpn/files/trafdump/add_trafdump_rules.sh
+++ ansible/roles/vpn/files/trafdump/add_trafdump_rules.sh
@@ -5,16 +5,16 @@
 sysctl net.core.rmem_max=2000000000
 echo "sysctl net.core.rmem_max=2000000000"
 
-#for chain in INPUT FORWARD OUTPUT; do
-#    if ! iptables -t mangle -C $chain -s 10.80.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
-#        iptables -t mangle -A $chain -s 10.80.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
-#    fi
-#
-#    if ! iptables -t mangle -C $chain -s 10.60.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
-#        iptables -t mangle -A $chain -s 10.60.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
-#    fi
-#
-#    if ! iptables -t mangle -C $chain -s 10.10.10.0/28 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
-#        iptables -t mangle -A $chain -s 10.10.10.0/28 -j NFLOG --nflog-group 1 --nflog-threshold 100
-#    fi
-#done
+for chain in INPUT FORWARD OUTPUT; do
+    if ! iptables -t mangle -C $chain -s 10.80.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
+        iptables -t mangle -A $chain -s 10.80.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
+    fi
+
+    if ! iptables -t mangle -C $chain -s 10.60.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
+        iptables -t mangle -A $chain -s 10.60.0.0/14 -j NFLOG --nflog-group 1 --nflog-threshold 100
+    fi
+
+    if ! iptables -t mangle -C $chain -s 10.10.10.0/28 -j NFLOG --nflog-group 1 --nflog-threshold 100 &>/dev/null; then
+        iptables -t mangle -A $chain -s 10.10.10.0/28 -j NFLOG --nflog-group 1 --nflog-threshold 100
+    fi
+done
