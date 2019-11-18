126acb3c48336eae193508594a77f17f05651d1b
SvetlanaNesterova <nesterova.svetlanka@yandex.ru>
2019-11-18 23:04:53 +0500

    openssl

--- services/Household/Household/Utils/CertificateLoader.cs
+++ services/Household/Household/Utils/CertificateLoader.cs
@@ -24,34 +24,64 @@ namespace Household.Utils
 
         public static ECDsaSecurityKey GetECDsaSecurityKey(string pfxPassword)
         {
-            var pfxPath = @".\keys.pfx";
-            var cerPath = @".\keys.cer";
-            if (!File.Exists(pfxPath)) //если нет приватного ключа создаем ключ
-            {
-                var createdDsa = CreateECDKey(); //создаем новый ключ
-                ExportECDKey(createdDsa, pfxPath, pfxPassword, cerPath); // сохраняем в файловую систему
-                createdDsa.Dispose();
-            }
-
-            var dsa = ImportECDKey(pfxPath, pfxPassword);
-            return new ECDsaSecurityKey(dsa);
+            var createdDsa = CreateECDsaKey(); //создаем новый ключ
+            return new ECDsaSecurityKey(createdDsa);
+            //var pfxPath = @".\keys.pfx";
+            //var cerPath = @".\keys.cer";
+            //if (!File.Exists(pfxPath)) //если нет приватного ключа создаем ключ
+            //{
+            //    var createdDsa = CreateECDsaKey(); //создаем новый ключ
+            //    ExportECDsaKey(createdDsa, pfxPath, pfxPassword, cerPath); // сохраняем в файловую систему
+            //    createdDsa.Dispose();
+            //}
         }
 
-        private static ECDsaCng CreateECDKey(string KeyName = "Ключ шифрования", string keyAlias = "AdminKey")
+        //public static ECDsaSecurityKey GetECDsaSecurityKey(string pfxPassword)
+        //{
+        //    var pfxPath = @".\keys.pfx";
+        //    var cerPath = @".\keys.cer";
+        //    if (!File.Exists(pfxPath)) //если нет приватного ключа создаем ключ
+        //    {
+        //        var createdDsa = CreateECDKey(); //создаем новый ключ
+        //        ExportECDKey(createdDsa, pfxPath, pfxPassword, cerPath); // сохраняем в файловую систему
+        //        createdDsa.Dispose();
+        //    }
+
+        //    var dsa = ImportECDKey(pfxPath, pfxPassword);
+        //    return new ECDsaSecurityKey(dsa);
+        //}
+
+        //private static ECDsaCng CreateECDKey(string KeyName = "Ключ шифрования", string keyAlias = "AdminKey")
+        //{
+        //    var keyCreationParameters = new CngKeyCreationParameters
+        //    {
+        //        ExportPolicy = CngExportPolicies.AllowExport,
+        //        KeyUsage = CngKeyUsages.Signing,
+        //        KeyCreationOptions = CngKeyCreationOptions.OverwriteExistingKey,
+        //        UIPolicy = new CngUIPolicy(CngUIProtectionLevels.ProtectKey, KeyName, "Key for signing tokens", null, "Creation title!"),
+        //    };
+        //    var key = CngKey.Create(CngAlgorithm.ECDsaP521, keyAlias, keyCreationParameters);
+        //    var dsa = new ECDsaCng(key)
+        //    {
+        //        HashAlgorithm = CngAlgorithm.Sha512
+        //    };
+        //    return dsa;
+        //}
+
+        public static ECDsa CreateECDsaKey()
         {
-            var keyCreationParameters = new CngKeyCreationParameters
-            {
-                ExportPolicy = CngExportPolicies.AllowExport,
-                KeyUsage = CngKeyUsages.Signing,
-                KeyCreationOptions = CngKeyCreationOptions.OverwriteExistingKey,
-                UIPolicy = new CngUIPolicy(CngUIProtectionLevels.ProtectKey, KeyName, "Key for signing tokens", null, "Creation title!"),
-            };
-            var key = CngKey.Create(CngAlgorithm.ECDsaP521, keyAlias, keyCreationParameters);
-            var dsa = new ECDsaCng(key)
-            {
-                HashAlgorithm = CngAlgorithm.Sha512
-            };
-            return dsa;
+            var ecDsa = new ECDsaOpenSsl();
+            ecDsa.GenerateKey(new ECCurve());
+            return ecDsa;
+        }
+
+        public static void ExportECDsaKey(ECDsa ecDsa, string pfxPath, string cerPath)
+        {
+            var privateKey = ecDsa.ExportECPrivateKey();
+            var publicKey = ecDsa.ExportSubjectPublicKeyInfo();
+
+            File.WriteAllBytes(pfxPath, privateKey);
+            File.WriteAllBytes(cerPath, publicKey);
         }
 
         private static void ExportECDKey(ECDsaCng dsa, string pfxPath, string pfxPassword, string cerPath)
