9c08ba49a10cf1aef1666495928bbdd8a11f7e4a
zarix908 <daniyarrus@gmail.com>
2019-11-18 05:17:34 +0500

    style input

--- services/staff/src/client/components/Input/Input.jsx
+++ services/staff/src/client/components/Input/Input.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import s from './Input.css';
 
 export function Input ({ onChange, value }) {
     const changeHandler = (e) => onChange(e.target.value);
 
     return (
-        <input
-            value={value}
-            onChange={changeHandler}
-        />
+        <div>
+            <input
+                value={value}
+                onChange={changeHandler}
+                className={s.input}
+            />
+        </div>
     );
 }
 
--- services/staff/src/client/pages/Login/Login.jsx
+++ services/staff/src/client/pages/Login/Login.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { observer } from 'mobx-react';
 import { login } from '../../models/login';
+import { Input } from '../../components/Input/Input';
+import s from './Login.css';
 
-const LoginC = observer(login => {
+const LoginComponent = observer(login => {
     return (
-        <input
-            value={login.username}
-            onChange={login.changeUsername}
-        />
+        <article className={s.login}>
+            <section>
+                <Input
+                    value={login.username}
+                    onChange={login.changeUsername}
+                />
+            </section>
+        </article>
     );
 });
 
 export const Login = () => (
-    <LoginC login={login} />
+    <LoginComponent login={login} />
 );
