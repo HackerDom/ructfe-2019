5201c452b98e349153cbc328bf5bdf20589df65a
1MiKHalyCH1 <bykoun@gmail.com>
2019-11-21 23:34:06 +0500

    profile checker

---
+++ checkers/profile/algos/stop/stop.py
@@ -0,0 +1,73 @@
+__name__ = 'Secure Trading Operation protocol'
+
+from abc import ABC, abstractmethod
+from hashlib import sha256
+from math import ceil, floor, log2
+from os import urandom
+
+from algos.stop.utils import bytes_to_bits, join_signature, split_signature, n_hash, split_message
+
+
+class STOp(ABC):
+    @property
+    def pub_key(self):
+        return self._pub_key
+
+    @abstractmethod
+    def generate_keys(self):
+        pass
+
+    @abstractmethod
+    def sign(self, msg, priv):
+        pass
+
+    @abstractmethod
+    def verify(self, msg, pub, sign):
+        pass
+
+class Nitzerwint(STOp):
+    def __init__(self, size=16):
+        self._bit_size = 8*size
+        self._byte_size = size
+        self._w = 8
+
+        t1 = ceil(self._bit_size / self._w)
+        t2 = ceil((floor(log2(t1)) + 1 + self._w) / self._w)
+        self._t = int(t1 +  t2)
+
+    def generate_keys(self):
+        priv_key = [urandom(self._byte_size) for _ in range(self._t)]
+        pub_key = [n_hash(x, sha256, 2**self._w - 1) for x in priv_key]
+        return pub_key, priv_key
+
+    def sign(self, msg, priv):
+        signature = [n_hash(x, sha256, y) for x, y in zip(priv, split_message(msg, self._w, self._t))]
+        return join_signature(signature)
+
+    def verify(self, msg, pub, sign):
+        sign = split_signature(sign, self._byte_size*2)
+        return all(x == n_hash(y, sha256, 2**self._w - 1 - z) for x,y,z in zip(pub, sign, split_message(msg, self._w, self._t)))
+
+class Lampfie(STOp):
+    def __init__(self, size=16):
+        self._bit_size = 8*size
+        self._byte_size = size
+
+    def generate_keys(self):
+        priv_key = [urandom(self._byte_size) for _ in range(self._bit_size*2)]
+        pub_key = [sha256(x).digest() for x in priv_key]
+        return pub_key, priv_key
+
+    def sign(self, msg, priv):
+        signature = []
+        for i, j in enumerate(bytes_to_bits(msg, self._bit_size)):
+            signature.append(priv[2*i + j])
+        
+        return join_signature(signature)
+
+    def verify(self, msg, pub, sign):
+        sign = [sha256(x).digest() for x in split_signature(sign, self._byte_size)]
+        for i, j in enumerate(bytes_to_bits(msg, self._bit_size)):
+            if pub[2*i+j] != sign[i]:
+                return False
+        return True
---
+++ checkers/profile/checker.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3.7
+import os
+import traceback
+from base64 import b64decode, b64encode
+
+import asyncio
+
+from api import API
+from algos.lwe.rlwe import RLWE
+from algos.stop.stop import Nitzerwint
+from chklib import Checker, CheckRequest, GetRequest, PutRequest, Verdict
+
+checker = Checker()
+
+
+@checker.define_check
+async def check_service(request: CheckRequest) -> Verdict:
+    async with API(request.hostname) as api:
+        try:
+            await api.health_check()
+        except Exception as ex:
+            return Verdict.DOWN('', traceback.format_exc())
+    return Verdict.OK()
+
+
+@checker.define_put(vuln_num=1, vuln_rate=1)
+async def put_flag_lwe(request: PutRequest) -> Verdict:
+    login = os.urandom(5).hex()
+    algo = 'lwe'
+
+    async with API(request.hostname) as api:
+        try:
+            pub_key = await api.get_pub_key(algo, login)
+            pub_key = list(map(int, b64decode(pub_key['pub_key']).decode().split(',')))
+        except Exception as e:
+            return Verdict.MUMBLE("Can't get public key", traceback.format_exc())
+    
+        try:
+            signed = await api.sign(algo, login, request.flag)
+            signature, note_hash = signed['s'], signed['h']
+ 
+            _signature = list(list(map(int, x.split(','))) for x in b64decode(signature).decode().split(';'))
+            _note_hash = bytes.fromhex(note_hash)
+        except Exception as e:
+            return Verdict.MUMBLE("Can't sign message", traceback.format_exc())
+
+    algo = RLWE(16, 929, 5)
+    if algo.verify(_note_hash, pub_key, _signature):
+        return Verdict.OK("{}:{}:{}".format(login, note_hash, signature))
+    else:
+        return Verdict.MUMBLE('Wrong signature', 'Wrong signature')
+
+
+@checker.define_get(vuln_num=1)
+async def get_flag_lwe(request: GetRequest) -> Verdict:
+    login, note_hash, signature = request.flag_id.split(':')
+    algo = 'lwe'
+
+    async with API(request.hostname) as api:
+        try:
+            users = await api.get_users()
+            if login not in users['users']:
+                return Verdict.MUMBLE('No user', 'No user')
+        except Exception as e:
+            return Verdict.MUMBLE("Can't get users", traceback.format_exc())
+
+        try:
+            notes = await api.get_notes(algo, login)
+            if note_hash not in notes['notes']:
+                return Verdict.MUMBLE('No note', 'No note')
+        except Exception as e:
+            return Verdict.MUMBLE("Can't get notes", traceback.format_exc())
+
+        try:
+            pub_key = await api.get_pub_key(algo, login)
+            pub_key = list(map(int, b64decode(pub_key['pub_key']).decode().split(',')))
+        except Exception as e:
+            return Verdict.MUMBLE("No public key", traceback.format_exc())
+        
+        try:
+            verified = await api.verify(algo, login, signature, note_hash)
+            flag = verified['data']
+        except Exception as e:
+            return Verdict.MUMBLE("Can't validate", traceback.format_exc())
+
+        if flag != request.flag:
+            return Verdict.MUMBLE('Wrong data')
+        return Verdict.OK()
+
+
+@checker.define_put(vuln_num=2, vuln_rate=1)
+async def put_flag_stop(request: PutRequest) -> Verdict:
+    login = os.urandom(5).hex()
+    algo = 'stop'
+
+    async with API(request.hostname) as api:
+        try:
+            pub_key = await api.get_pub_key(algo, login)
+            pub_key = list(map(bytes.fromhex, b64decode(pub_key['pub_key']).decode().split(',')))
+        except Exception as e:
+            return Verdict.MUMBLE("Can't get public key", traceback.format_exc())
+    
+        try:
+            signed = await api.sign(algo, login, request.flag)
+            signature, note_hash = signed['s'], signed['h']
+ 
+            _signature = bytes.fromhex(signature)
+            _note_hash = bytes.fromhex(note_hash)
+        except Exception as e:
+            return Verdict.MUMBLE("Can't sign message", traceback.format_exc())
+
+    algo = Nitzerwint()
+
+    if algo.verify(_note_hash, pub_key, _signature):
+        return Verdict.OK("{}:{}:{}".format(login, note_hash, signature))
+    else:
+        return Verdict.MUMBLE('Wrong signature', 'Wrong signature')
+
+
+@checker.define_get(vuln_num=2)
+async def get_flag_stop(request: GetRequest) -> Verdict:
+    login, note_hash, signature = request.flag_id.split(':')
+    algo = 'stop'
+
+    async with API(request.hostname) as api:
+        try:
+            users = await api.get_users()
+            if login not in users['users']:
+                return Verdict.MUMBLE('No user', 'No user')
+        except Exception as e:
+            return Verdict.MUMBLE("Can't get users", traceback.format_exc())
+
+        try:
+            notes = await api.get_notes(algo, login)
+            if note_hash not in notes['notes']:
+                return Verdict.MUMBLE('No note', 'No note')
+        except Exception as e:
+            return Verdict.MUMBLE("Can't get notes", traceback.format_exc())
+
+        try:
+            pub_key = await api.get_pub_key(algo, login)
+            pub_key = list(map(bytes.fromhex, b64decode(pub_key['pub_key']).decode().split(',')))
+        except Exception as e:
+            return Verdict.MUMBLE("No public key", traceback.format_exc())
+        
+        try:
+            verified = await api.verify(algo, login, signature, note_hash)
+            flag = verified['data']
+        except Exception as e:
+            return Verdict.MUMBLE("Can't validate", traceback.format_exc())
+
+        if flag != request.flag:
+            return Verdict.MUMBLE('Wrong data')
+        return Verdict.OK()
+
+
+if __name__ == "__main__":
+    checker.run()
